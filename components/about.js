4c2a99e522eafdf8407595f91e229478b7e91f15
Daniel Errington <derrington@gmail.com>
2026-02-17 13:57:59 +0000

    Update e-mail address to reflect domain name change

--- components/about.js
+++ components/about.js
@@ -21,7 +21,7 @@ const AboutModal = ({ open, onClose }) => {
                     Our aim is to list all active flat-track, WFTDA rules roller derby leagues in the UK and show where they train or play to make it easier to find teams near you to join or play against.
                 </Typography>
                 <Typography sx={{ mt: 2 }}>
-                    The list has been compiled and refined over several years and we believe it to be a fairly accurate picture of roller derby in the UK. However, if you notice any discrepancies or would like to bring a new league to our attention, please e-mail <Link href="mailto;contact@rollerderbyleagues.co.uk">contact@rollerderbyleagues.co.uk</Link> with the details.
+                    The list has been compiled and refined over several years and we believe it to be a fairly accurate picture of roller derby in the UK. However, if you notice any discrepancies or would like to bring a new league to our attention, please e-mail <Link href="mailto:contact@ukderbyleagues.com">contact@ukderbyleagues.com</Link> with the details.
                 </Typography>
                 <Typography sx={{ mt: 2 }}>
                     The list is licensed under the <Link href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL General Public License v3</Link>.
